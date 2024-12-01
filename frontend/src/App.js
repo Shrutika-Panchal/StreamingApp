64583128d9aa916d15b51f2a4ff1aae15dc67dc5
Shrutika Panchal <panchalshrutika00@gmail.com>
2024-12-01 21:01:07 +0530

    Update StreamingApp directory with changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
     <h1>HotPrimeFlix</h1>
      <video id="videoPlayer" width="70%" controls autoplay>
-            <source src="localhost:3002/streaming" type="video/mp4" />
+            <source src="http://a1817e72fc09e4b5fa44cae96dd90f71-943234011.ap-northeast-3.elb.amazonaws.com:3002/static/video.mp4" type="video/mp4" />
         </video>
     </div>
   );
